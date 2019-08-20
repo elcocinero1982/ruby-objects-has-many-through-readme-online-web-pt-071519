12f765bfc9401e113cd4c807ecceb471b8f3e8f9
jose luis <cortezluisjose@gmail.com>
2019-08-20 03:43:54 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -12,10 +12,7 @@ class Waiter
   def self.all
     @@all
   end
- def best_tipper
-  best_tipped_meal = meals.max do |meal_a, meal_b|
-    meal_a.tip <=> meal_b.tip
-  end
+ 
   def new_meal(waiter, total, tip=0)
     Meal.new(waiter, self, total, tip)
   end
